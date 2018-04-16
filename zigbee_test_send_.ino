42745f7cdf8e2f574e4a42ee27a7a4bbf86973fe
LeeJeho <leejen2003@naver.com>
2018-04-16 21:06:52 +0900

    write Send code(test)

--- zigbee_test_send_.ino
+++ zigbee_test_send_.ino
@@ -1,11 +1,14 @@
 #include <SoftwareSerial.h>
 
+SoftwareSerial xbee(2, 3);
+
 void setup() {
   // put your setup code here, to run once:
-
+  xbee.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  xbee.print('A');
+  delay(1000);
 }
