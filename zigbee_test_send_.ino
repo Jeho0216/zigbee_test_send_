2e5f3d8c4d55ec8c11285561f8c227f6f7f2d0f9
LeeJeho <leejen2003@naver.com>
2018-04-22 02:23:25 +0900

    솔루션 생성 / 전송코드 작성

--- zigbee_test_send_.ino
+++ zigbee_test_send_.ino
@@ -3,13 +3,36 @@
 SoftwareSerial xbee(2, 3);
 
 void setup() {
-  // put your setup code here, to run once:
 	Serial.begin(9600);
 	xbee.begin(9600);
+	char type = '1';
+	int data = 1234;
+	//char send[10];
+	String send_data;
+	send_data = String(type) + "," + String(data) + ";";
+	Serial.println(send_data);
+	//send_data.toCharArray(send, send_data.length());
+	xbee.println(send_data);
+}
+
+String readSerial() {
+	String str = "";
+	char ch;
+	if (xbee.available()) {
+		while (xbee.available() > 0) {
+			ch = xbee.read();
+			str.concat(ch);
+			delay(10);
+		}
+	}
+	return str;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-	xbee.write("A\n");
+	String str;
+	str = readSerial();
+	if(str != "")
+		Serial.println(str);
+	Serial.println("I am keep ruuning!");
 	delay(1000);
 }
