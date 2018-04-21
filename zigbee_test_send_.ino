ad8e6f67befe1f432bdfaf15a3831df9db1d750f
LeeJeho <leejen2003@naver.com>
2018-04-21 22:41:53 +0900

    xbee 코드 변경(print -> write)

--- zigbee_test_send_.ino
+++ zigbee_test_send_.ino
@@ -4,11 +4,12 @@ SoftwareSerial xbee(2, 3);
 
 void setup() {
   // put your setup code here, to run once:
-  xbee.begin(9600);
+	Serial.begin(9600);
+	xbee.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  xbee.print('A');
-  delay(1000);
+	xbee.write("A\n");
+	delay(1000);
 }
