3e5e2c70679e688ea654aca6cb11ce2fe4ebd262
LeeJeho <leejen2003@naver.com>
2018-04-23 15:14:16 +0900

    난수 생성/ 전송

--- zigbee_test_send_.ino
+++ zigbee_test_send_.ino
@@ -5,14 +5,9 @@ SoftwareSerial xbee(2, 3);	//지그비와 아두이노 연결.
 void setup() {
 	Serial.begin(9600);		//시리얼모니터 사용
 	xbee.begin(9600);		//지그비 통신속도
-	
-	char type = '1';		//차후에 센서값 구분을 위한 변수
-	int data = 1234;		//센서값에 해당하는 변수
 
-	String send_data;		//라즈베리파이로 전송하기 위한 센서타입, 센서값 합치는 변수
-	send_data = String(type) + "," + String(data) + ";";	//합치기
-	Serial.println(send_data);		//시리얼모니터로 출력
-	xbee.println(send_data);		//지그비로 전송
+	Serial.println("Arduino Ready.");
+	xbee.print("Arduino Ready.");
 }
 
 //라즈베리파이로 부터 수신받는 경우, 문자열 출력을 위한 부분
@@ -30,10 +25,25 @@ String readSerial() {
 }
 
 void loop() {
-	String str;
-	str = readSerial();
-	if(str != "")		//수신된 내용이 존재할 경우에 수행.
-		Serial.println(str);
-	Serial.println("I am keep ruuning!");
-	delay(1000);
+	String receive_str;
+	long value = random(0, 256);	//0~255까지의 난수 발생.
+	char type = '1';		//차후에 센서값 구분을 위한 변수
+	String send_data;		//라즈베리파이로 전송하기 위한 센서타입, 센서값 합치는 변수
+	boolean connect_raspi = false;
+
+	delay(5000);
+	receive_str = readSerial();
+	if (receive_str != "")		//수신된 내용이 존재할 경우에 수행.
+		Serial.println(receive_str);
+	//라즈베리파이와 연결된 상태인지 확인.
+
+	send_data = String(type) + "," + String(value) + ";";	//합치기
+	Serial.println("Send data : " + send_data);
+	xbee.println(send_data);		//지그비로 전송
+
+	if(connect_raspi == false){
+		Serial.println("not connect to Raspberry Pi");
+	}
+
+	//수신된 내용이 존재하지 않을 경우,
 }
